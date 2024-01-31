8742bed2143615d69b1eafb2f0d480855741f4df
vu <quachvotinh005@gmail.com>
2024-01-31 17:25:47 +0900

    adda

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Sử dụng một hình ảnh chứa Node.js
+FROM node:16
+
+# Tạo thư mục làm việc trong container
+WORKDIR /usr/src/app
+
+# Sao chép file package.json và package-lock.json (nếu có) để cài đặt dependencies
+COPY package*.json ./
+
+# Cài đặt dependencies
+RUN npm install
+
+# Sao chép tất cả các file trong dự án của bạn vào thư mục làm việc trong container
+COPY . .
+
+# Mở cổng 3000
+EXPOSE 3000
+
+# Lệnh để chạy ứng dụng Node.js (đảm bảo rằng file chính là index.js)
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser'); // Import thư viện cookie-parser
+
+const app = express();
+const port = 3000;
+
+// Sử dụng cookie-parser middleware để xử lý cookie
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  // Đặt cookie với nội dung "hello" và thuộc tính httpOnly là true
+  res.cookie('myCookie', 'helloccacaascsa', { httpOnly: true });
+
+  res.send('Hello, World! Ứng dụng Express đang chạy trên cổng 3000.');
+});
+
+app.listen(port, () => {
+  console.log(`Ứng dụng Express đang lắng nghe trên cổng ${port}`);
+});
